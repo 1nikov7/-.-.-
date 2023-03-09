a75fb70cc6cc68bd31ce588813d5fd5d834f8ec8
Никита Шипачев <114631813+1nikov7@users.noreply.github.com>
2023-03-09 10:11:55 +0700

    Add files via upload

---
+++ 2 полугодие/[etnf.py
@@ -0,0 +1,30 @@
+if (not(y<=x) or (z<=w) or not(w)==false: # Задача номер 2
+
+    break
+
+chislo = '10' + num[2:]+'0'                          #Задача номер 5
+
+goto(x=30,y=30)                                          # Задача номер 6
+    dot(2) # размер точки
+
+product                                                          # Задача номер 8
+n = '16 36 56 76 61 63 65 67'
+if numb.count(6)==1 and numb[0]!= '0':
+    for...:
+        if x in numb:
+            sp.append(x)
+        if not sp:
+            k+=1
+
+if all(num % dif for delit(0) in range (2,num)) :          #Задача номер 12
+    if y*4x//7 in spisok:
+        print(y)
+
+
+f = int(f'123{x}5',15) + int(f'1{x}233',15)                       # Задача номер 14
+if f % 14 ==0:
+    print(f)
+
+
+if (all((x%2==0) <= ( x%3!=0)) or (x+a>=100) for x in range (1,1000)):    # Задача номер 15
+
