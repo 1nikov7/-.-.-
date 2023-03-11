a8f63908adcd253d9d74032ff9bff7c63eb66a95
Никита Шипачев <114631813+1nikov7@users.noreply.github.com>
2023-03-11 11:53:21 +0700

    Update and rename [etnf.py to справочник.py

---
+++ 2 полугодие/справочник.py
@@ -0,0 +1,92 @@
+# задача номер 2
+
+if (not(y<=x) or (z<=w) or not(z))==False: # if - условие, not - инверсия, or - или, <= - импликация, == - равенство
+
+# задача номер 5
+
+num=(bin(i)[2:])
+
+num.count('1')%2==0 
+
+# зедача номер 6 
+from turtle import * # Ипотируется Черепашка
+
+left(90) # поворот влево 
+
+forward(300) # продвижение вперёд 
+
+right(120) # поворот вправо 
+
+pu() # поднятие черепахи
+
+goto(x*30,y*30) # перемещение черепахи по x и y
+
+dot(5) # размер точки
+
+done() # окно остаётся открытым
+
+#8
+
+from itertools import product
+
+n='16 36 56 76 61 63 65 67'
+
+if numb.count('6')==1 and numb[0]!='0':
+        for x in nn:
+            if x in numb:
+                sp.append(x)
+        if nos sp:
+            k+=1
+    
+
+    #Задача номер 12
+# условие простого числа
+if all(num%delit!=0 for delit in range(2,num))
+if y*4x//7 in spisok
+s.replace('1', '2')
+
+
+         # Задача номер 14
+f = int(f'123{x}5',15) + int(f'1{x}233',15)                     
+if f % 14 ==0:
+    print(f)
+
+
+     # Задача номер 15
+if (all((x%2==0) <= ( x%3!=0)) or (x+a>=100) for x in range (1,1000)):   
+
+
+#задача номер 16
+def f(n):
+    if n == 0: return 1
+    return f(n-1)+1
+
+    
+    #задача номер 17
+#работа с парами
+
+with open('17.txt') as f: # импорт элементов из файла
+
+if abs(a[i]%10==3 and a[i+1]%10!=3) or abs(a[i]%10!=3 and a[i+1]%10==3):
+    
+#задача номер23
+
+from itertools import product # импорт метода из библиотеки
+
+return
+
+#задача номер 24
+
+s.replace('AG', *) # замена одного на другое
+
+for i in s:
+  if i=='*'
+
+# задача номер 25
+
+if s[0]=='1' and s[2] == '2' and s[3] == '1' and s[4] == '3' and s[5] == '9' and s[-1] == '4':
+
+# задача номер 26
+
+ЕСЛИ(D3+C2>0;0;D3+C2)
+СЧЁТЕСЛИ(...)
